84b6d9201c119798529ab9e959963d6a47ff44ba
Jinze <shajinze@outlook.com>
2019-11-05 23:32:18 +0000

    Added compiled result to git repo

---
+++ build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/home/js2294/Warp-firmware/build/ksdk1.1/work/platform/utilities/src/fsl_misc_utilities.c.d
@@ -0,0 +1,14 @@
+CMakeFiles/Warp.dir/home/js2294/Warp-firmware/build/ksdk1.1/work/platform/utilities/src/fsl_misc_utilities.c.obj: \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/platform/utilities/src/fsl_misc_utilities.c \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/utilities/inc/fsl_misc_utilities.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/utilities/inc/fsl_debug_console.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/osa/inc/fsl_os_abstraction.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/osa/inc/fsl_os_abstraction_bm.h
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/utilities/inc/fsl_misc_utilities.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/utilities/inc/fsl_debug_console.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/osa/inc/fsl_os_abstraction.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/osa/inc/fsl_os_abstraction_bm.h:
---
+++ build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/CMakeFiles/KsdkPlatformLib.dir/home/js2294/Warp-firmware/build/ksdk1.1/work/platform/hal/src/mcglite/fsl_mcglite_hal_modes.c.d
@@ -0,0 +1,41 @@
+CMakeFiles/KsdkPlatformLib.dir/home/js2294/Warp-firmware/build/ksdk1.1/work/platform/hal/src/mcglite/fsl_mcglite_hal_modes.c.obj: \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/platform/hal/src/mcglite/fsl_mcglite_hal_modes.c \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/hal/inc/fsl_mcglite_hal.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/fsl_device_registers.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cm0plus.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cmInstr.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cmFunc.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/startup/MKL03Z4/system_MKL03Z4.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_registers.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/fsl_bitaccess.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_features.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/hal/inc/fsl_mcglite_hal_modes.h \
+ /home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/hal/inc/fsl_mcglite_hal.h
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/hal/inc/fsl_mcglite_hal.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/fsl_device_registers.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cm0plus.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cmInstr.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/core_cmFunc.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/startup/MKL03Z4/system_MKL03Z4.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_registers.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/fsl_bitaccess.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_features.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/hal/inc/fsl_mcglite_hal_modes.h:
+
+/home/js2294/Warp-firmware/build/ksdk1.1/work/lib/ksdk_platform_lib/armgcc/KL03Z4/../../../../platform/hal/inc/fsl_mcglite_hal.h:
