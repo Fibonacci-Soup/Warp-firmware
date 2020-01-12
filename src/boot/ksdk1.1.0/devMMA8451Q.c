6454520e52fa840e1b327ba3de4f97c76a21a6fa
Jinze Sha <shajinze@outlook.com>
2020-01-12 23:25:40 +0000

    CW5 Trial 8 Code

--- src/boot/ksdk1.1.0/devMMA8451Q.c
+++ src/boot/ksdk1.1.0/devMMA8451Q.c
@@ -306,30 +306,21 @@ startFreefallDetectionMMA8451Q(uint16_t menuI2cPullupValue){
 	WarpStatus i2cCommStatus;
 	uint8_t temp_data_read;
 	SEGGER_RTT_printf(0, "Start detecting fall");
-	//writeSensorRegisterMMA8451Q(kWarpSensorConfigurationRegisterMMA8451QF_SETUP, 0x00, menuI2cPullupValue);
-	SEGGER_RTT_printf(0, "WTF0");
 	i2cCommStatus = writeSensorRegisterMMA8451Q(0x2A, 0x00, menuI2cPullupValue); //Set the device in 50 Hz ODR, Standby
-	SEGGER_RTT_printf(0, "WTF1");
 	i2cCommStatus = writeSensorRegisterMMA8451Q(0x15, 0xB8, menuI2cPullupValue); //Configuration Register set for Freefall Detection enabling ¡°AND¡± condition, OAE = 0, Enabling X, Y, Z and the Latch
-	SEGGER_RTT_printf(0, "WTF2");
 	i2cCommStatus = writeSensorRegisterMMA8451Q(0x17, 0x03, menuI2cPullupValue); //Threshold Setting Value for the resulting acceleration (The step count is 0.063g/count)
-	SEGGER_RTT_printf(0, "WTF3");
 	i2cCommStatus = writeSensorRegisterMMA8451Q(0x18, 0x06, menuI2cPullupValue); //Set the debounce counter to eliminate false positive readings for 50Hz sample rate
-	SEGGER_RTT_printf(0, "WTF4");
 	i2cCommStatus = writeSensorRegisterMMA8451Q(0x2D, 0x04, menuI2cPullupValue);
-	SEGGER_RTT_printf(0, "WTF5");
 	i2cCommStatus = writeSensorRegisterMMA8451Q(0x2E, 0x00, menuI2cPullupValue);
-	SEGGER_RTT_printf(0, "WTF6");
 	i2cCommStatus = writeSensorRegisterMMA8451Q(0x2A, 0x01, menuI2cPullupValue); //Put the device in Active Mode
 	SEGGER_RTT_printf(0, "WTF7");
 	while (1){
-		SEGGER_RTT_printf(0, "WTF8");
 		readSensorRegisterMMA8451Q(0x16, 1);
-		SEGGER_RTT_printf(0, "WTF9");
 		temp_data_read = deviceMMA8451QState.i2cBuffer[0];
-		SEGGER_RTT_printf(0, "%d", temp_data_read);
-		temp_data_read &= 0x80;
-		if (temp_data_read == 0x80){
+		SEGGER_RTT_printf(0, "%d\n", temp_data_read);
+		temp_data_read &= 0x40;
+		SEGGER_RTT_printf(0, "P: %d\n", temp_data_read);
+		if (temp_data_read == 0x40){
 			SEGGER_RTT_printf(0, "Fall detected!!!");
 			break;
 		}
