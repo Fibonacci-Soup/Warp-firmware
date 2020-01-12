09f872fe23c9dedcc015874a2527fc07f7b27c88
Jinze Sha <shajinze@outlook.com>
2020-01-12 23:34:41 +0000

    CW5 Trial 9 Code

--- src/boot/ksdk1.1.0/devMMA8451Q.c
+++ src/boot/ksdk1.1.0/devMMA8451Q.c
@@ -317,10 +317,8 @@ startFreefallDetectionMMA8451Q(uint16_t menuI2cPullupValue){
 	while (1){
 		readSensorRegisterMMA8451Q(0x16, 1);
 		temp_data_read = deviceMMA8451QState.i2cBuffer[0];
-		SEGGER_RTT_printf(0, "%d\n", temp_data_read);
-		temp_data_read &= 0x40;
-		SEGGER_RTT_printf(0, "P: %d\n", temp_data_read);
-		if (temp_data_read == 0x40){
+		SEGGER_RTT_printf(0, "%x\n", temp_data_read);
+		if (temp_data_read > 0x7F){
 			SEGGER_RTT_printf(0, "Fall detected!!!");
 			break;
 		}
